afd3c729de5f5689ad9095b645e44022d55c870b
Angel Abellanosa <auabellanosa@hotmail.com>
2013-11-30 16:45:52 +0800

    adding backend support for login and register pages

--- src/com/appfibre/lifebeam/LifebeamApp.java
+++ src/com/appfibre/lifebeam/LifebeamApp.java
@@ -5,8 +5,8 @@ import android.app.Application;
 import com.appfibre.lifebeam.classes.Activity;
 import com.appfibre.lifebeam.classes.Family;
 import com.appfibre.lifebeam.classes.Invitee;
-import com.appfibre.lifebeam.classes.User;
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
 
 public class LifebeamApp extends Application{
@@ -19,11 +19,10 @@ public class LifebeamApp extends Application{
 		ParseObject.registerSubclass(Activity.class);
 		ParseObject.registerSubclass(Family.class);
 		ParseObject.registerSubclass(Invitee.class);
-		ParseObject.registerSubclass(User.class);
 		
 		Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
 		
-		//ParseFacebookUtils.initialize(getResources().getString(R.string.fbAppID));
+		ParseFacebookUtils.initialize(getResources().getString(R.string.fbAppID));
 	} 
 
 }
