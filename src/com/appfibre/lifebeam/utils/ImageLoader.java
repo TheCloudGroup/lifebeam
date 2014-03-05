3d9eaf4b0f9ae9193b0996593e110855c3aa4ec5
kimlambiguit <kimlambiguit@gmail.com>
2014-03-05 22:31:19 +0800

    fix for LB-52

--- src/com/appfibre/lifebeam/utils/ImageLoader.java
+++ src/com/appfibre/lifebeam/utils/ImageLoader.java
@@ -1,5 +1,6 @@
 package com.appfibre.lifebeam.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Map;
@@ -22,6 +24,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class ImageLoader {
@@ -96,70 +100,58 @@ public class ImageLoader {
     
     private Bitmap getBitmap(String url) 
     {
+    	Bitmap bm = null;
         File f=fileCache.getFile(url);
-        
-        //from SD cache
-        Bitmap b = decodeFile(f);
-        if(b!=null)
-            return b;
-        
-        //from web
-        try {
-            Bitmap bitmap=null;
-            URL imageUrl = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection)imageUrl.openConnection();
-            conn.setConnectTimeout(30000);
-            conn.setReadTimeout(30000);
-            conn.setInstanceFollowRedirects(true);
-            InputStream is=conn.getInputStream();
-            OutputStream os = new FileOutputStream(f);
-            Utils.CopyStream(is, os);
-            os.close();
-            bitmap = decodeFile(f);
-            return bitmap;
-        } catch (Throwable ex){
-           ex.printStackTrace();
-           if(ex instanceof OutOfMemoryError)
+        if(f.exists()){ //from SD cache
+        	bm = decodeFile(f);
+        } else {
+            try { //from web
+                URL imageUrl = new URL(url);
+                HttpURLConnection conn = (HttpURLConnection)imageUrl.openConnection();
+                conn.setConnectTimeout(30000);
+                conn.setReadTimeout(30000);
+                conn.setInstanceFollowRedirects(true);
+                InputStream is=conn.getInputStream();
+                OutputStream os = new FileOutputStream(f);
+                Utils.CopyStream(is, os);
+                os.close();
+                bm = decodeFile(f);
+            } catch( MalformedURLException e){
+            	Log.e("ImageLoader.getBitmap", e.getMessage());
+            } catch( IOException e){
+            	Log.e("ImageLoader.getBitmap", e.getMessage());
+            } catch (OutOfMemoryError e){
+            	Log.e("ImageLoader.getBitmap", e.getMessage());
                memoryCache.clear();
-           return null;
+            }
         }
+        
+        return bm;
     }
 
     //decodes image and scales it to reduce memory consumption
     private Bitmap decodeFile(File f){
+    	Bitmap bm = null;
         try {
-            //decode image size
-            BitmapFactory.Options o = new BitmapFactory.Options();
-            o.inJustDecodeBounds = true;
-            FileInputStream stream1=new FileInputStream(f);
-            BitmapFactory.decodeStream(stream1,null,o);
-            stream1.close();
-            
-            //Find the correct scale value. It should be the power of 2.
-            final int REQUIRED_SIZE=70;
-            int width_tmp=o.outWidth, height_tmp=o.outHeight;
-            int scale=1;
-            while(true){
-                if(width_tmp/2<REQUIRED_SIZE || height_tmp/2<REQUIRED_SIZE)
-                    break;
-                width_tmp/=2;
-                height_tmp/=2;
-                scale*=2;
-            }
-            
-            //decode with inSampleSize
-            BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize=scale;
-            FileInputStream stream2=new FileInputStream(f);
-            Bitmap bitmap=BitmapFactory.decodeStream(stream2, null, o2);
-            stream2.close();
-            return bitmap;
-        } catch (FileNotFoundException e) {
-        } 
-        catch (IOException e) {
-            e.printStackTrace();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 1;
+
+            //File f = new File(imagePath);
+            Bitmap bmp = BitmapFactory.decodeStream(new FileInputStream(f),
+                    null, options);
+            bm = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(),
+                    bmp.getHeight(), new Matrix(), true);
+
+            ByteArrayOutputStream baOutStream = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.PNG, 100,
+                    baOutStream);
+        } catch (IOException e) {
+            Log.e("ImageLoader.decodeFile", e.getMessage());
+        } catch (OutOfMemoryError e) {
+        	Log.e("ImageLoader.decodeFile", e.getMessage());
+        	memoryCache.clear();
         }
-        return null;
+        return bm;
     }
     
     //Task for the queue
